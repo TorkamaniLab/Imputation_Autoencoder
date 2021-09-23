5faf78bd3fa1a69338c444b41f827a395cdd0100
RaquelDias <raqueld@scripps.edu>
2021-09-23 15:28:58 -0700

    GPU efficiency improved

--- autoencoder_tuning_pipeline/1_train_GS.sh
+++ autoencoder_tuning_pipeline/1_train_GS.sh
@@ -10,13 +10,9 @@ indir=$1
 #3. parallelize all models across all GPUs in the whole node
 #4. verifies if the job was successful or failed and need to rerun, returning either 0 or non-zero status
 
-
-
+#1. check how much VRAM this model needs
 ngpus=$(nvidia-smi --query-gpu=gpu_name --format=csv | tail -n +2 | wc -l)
-
-
 nvar=$(cat $indir/NVAR)
-
 #identify max VRAM used per model and estimate how many models can run in parallel
 mem_per_proc=$(python3 tools/estimate_VRAM_needed_for_autoencoder.py $nvar | tail -n 1 | awk '{print $NF}' | sed -e 's/MiB//g')
 echo -e "$mem_per_proc" > $indir/VRAM
@@ -29,26 +25,26 @@ if ! [[ $mem_per_proc =~ $re ]] ; then
     echo -e "error: estimate_VRAM_needed_for_autoencoder.py failed, the models probably don't fit in VRAM, check error log, max VRAM available: $max_mem_avail"; exit 1
 elif [ $npar -lt 1 ]; then
     echo -e "error: estimate_VRAM_needed_for_autoencoder.py failed, the models probably don't fit in VRAM, check error log, max VRAM available: $max_mem_avail"; exit 1
-elif [ $npar -gt 6 ]; then
-    echo -e "Maximum theoretical number of processes per GPU is $npar, but it will be reduced to 6 to avoid I/O overhangs and RAM/CPU bottleneck"
-    npar=6
 fi
 
 echo -e "mem_per_proc = $mem_per_proc; max_mem_avail = $max_mem_avail; npar = $npar"
 
 echo -e "cd $indir;"
-cd $indir; 
+cd $indir;
 
 batchi=$(cat BATCH_ID)
 input=$(cat INPUT)
+mem_needed=$(cat VRAM)
+pids=${batchi}.PID_list.txt
+if [ -f $pids ]; then
+    rm $pids
+fi
+
 
 echo -e "batchi = $batchi; nvar = $nvar; input = $input"
 
 #split data across $ngpus GPUs
 nhp=$(cat $batchi | wc -l)
-chunksize=$(echo -e "$nhp/$ngpus" | bc)
-split -l $chunksize -d -a 1 $batchi $batchi.GPU
-mgpu=$((ngpus-1))
 
 echo -e "nhp = $nhp; chunksize = $chunksize; mgpu = $mgpu"
 
@@ -67,18 +63,69 @@ else
     echo "Not using A100, nor V100 flags ${2}"
 fi
 
-for i in $(seq 0 1 $mgpu); do
-    sed -i -e "s/<my_GPU_id>/$i/g" $batchi.GPU$i
-    sed -i -e "s/CUDA_VISIBLE_DEVICES=[0-9]/CUDA_VISIBLE_DEVICES=$i/g" $batchi.GPU$i
 
-    echo -e "${GPU_flags[${i}]}parallel -j $npar < $batchi.GPU$i"
-done > run.sh
-
-#run $ngpus GPUs in parallel
 gsstarttime=$(date +%s)
 
-echo -e "parallel -j $ngpus < run.sh"
-parallel -j $ngpus < run.sh
+while read train_script; do
+    result=-1
+    echo $train_script
+    input_file=$(echo $train_script | sed -e 's/.*--input //g' | sed -e 's/ .*//g')
+    model_id=$(echo $train_script | sed -e 's/.*--model_id //g' | sed -e 's/ .*//g')
+    echo $train_script > train_${model_id}.sh
+
+    while [ $result -eq -1 ]; do
+        result=-1
+        echo "Submitting ${train_script}"
+        echo -e "Input file: $input_file"
+
+        #2. check the VRAM still available in GPUs
+        gpui_mem=$(nvidia-smi --query-gpu=gpu_name,memory.free --format=csv | tail -n +2 | awk '{print NR-1 "\t" $(NF-1)}' | sort -k2g -k1gr | tail -n 1)
+        gpui=$(echo $gpui_mem | awk '{print $1}')
+        mem_avail=$(echo $gpui_mem | awk '{print $2}')
+        echo "GPU available: $gpui, memory available: $mem_avail, memory needed: $mem_needed"
+        #3. there is room in the VRAM for this model?
+        if [ $mem_avail -gt $mem_needed ]; then
+            #4. if yes, submit the model to GPUi
+            #first time
+            sed -i -e "s/<my_GPU_id>/$gpui/g" train_${model_id}.sh
+            #retraining failed job
+            sed -i -e "s/CUDA_VISIBLE_DEVICES=[0-9] /CUDA_VISIBLE_DEVICES=$gpui /g" train_${model_id}.sh
+            ${GPU_flags[${gpui}]}bash train_${model_id}.sh &
+            echo -e "${GPU_flags[${gpui}]}bash train_${model_id}.sh"
+            PID=$!
+            result=$PID
+        fi
+        #no GPUs have VRAM available, wait 10 minutes
+        #5. if none of all GPUs is available, wait and try again later
+        if [ $result -eq -1 ]; then
+            sleep 600
+        fi
+
+
+        if [ $result -ne -1 ]; then
+            echo "Submited $indir/train_${model_id}.sh, PID $result"
+            echo "Script content:"
+            cat train_${model_id}.sh
+            echo "Directory: $indir"
+            echo -e "$result" >> $pids
+            #allow some time to the job allocate the whole VRAM it needs, then go to the next
+            sleep 120
+        fi
+    done
+done < $batchi
+
+while true; do
+    if [ -s $pids ] ; then
+        for pid in $(cat $pids); do
+            echo "Checking pids $pid"
+            kill -0 "$pid" 2>/dev/null || sed -i "/^$pid$/d" $pids
+        done
+    else
+        echo "All process completed"
+        break
+    fi
+    sleep 180;
+done
 
 gsendtime=$(date +%s)
 
@@ -86,6 +133,10 @@ gsruntime=$((gsendtime-gsstarttime))
 
 
 echo "GS run time: $gsruntime"
+
+
+
+
 
 nmodels=$(ls -l IMPUTATOR_*/*.pth | wc -l)
 if [ $nmodels -lt 90 ]; then
